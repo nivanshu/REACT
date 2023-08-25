8ba9c161f7604628c697730fa5828cd4277a0dda
nivanshu <nivanshugupta0123@gmail.com>
2023-08-25 17:09:15 +0530

    day 2 react

--- src/component/ImgCard.jsx
+++ src/component/ImgCard.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+
 const ImgCard = (props) => {
+  const [count , setcount] = useState(0);
+const Handledclick=()=>{
+  setcount(count+1)
+  console.log("button clicked",count);
+};
   return (
     <div>
        <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={props.imgUrl}/>
       <Card.Body>
-        <Card.Title>{props.Title}</Card.Title>
+        <Card.Title>{props.title} {count}</Card.Title>
         <Card.Text>
           {props.desc}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button onClick ={Handledclick}  variant="primary" >Go somewhere</Button>
       </Card.Body>
     </Card>
     </div>
